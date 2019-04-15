47c3252280321f9a97ad9950b006426c0f4b5ed2
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-15 13:44:15 +0100

    added soltion

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,16 @@
 def clock_angle(time)
-    # code your solution here
+    time_elements = time.split(":")
+    time_elements.map! { |string| string.to_f }
+
+    min_hand = time_elements[1] * 6
+    hour_hand_one = time_elements[0] * 30
+      if hour_hand_one == 360
+        hour_hand_one = 0
+      end
+    hour_hand_two = time_elements[1] * 0.5
+    hour_hand = hour_hand_one + hour_hand_two
+
+    degree_1 = (hour_hand - min_hand).abs
+    degree_2 = 360 - degree_1
+    [degree_1, degree_2].min
 end
